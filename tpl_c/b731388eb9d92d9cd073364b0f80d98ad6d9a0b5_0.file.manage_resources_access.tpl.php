e68870b5d6fb472afdb882c9c29e6d9969b79bd5
Suthep Chanchuphol <suthep.chanchuphol@outlook.com>
2020-11-10 07:05:16 +0700

    Update equipment management page

--- tpl_c/b731388eb9d92d9cd073364b0f80d98ad6d9a0b5_0.file.manage_resources_access.tpl.php
+++ tpl_c/b731388eb9d92d9cd073364b0f80d98ad6d9a0b5_0.file.manage_resources_access.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2020-11-10 05:14:47
+/* Smarty version 3.1.30, created on 2020-11-10 07:04:35
   from "/var/www/html/booking/tpl/Admin/Resources/manage_resources_access.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5fa9bf57661a60_54977481',
+  'unifunc' => 'content_5fa9d913824029_51721097',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'b731388eb9d92d9cd073364b0f80d98ad6d9a0b5' => 
     array (
       0 => '/var/www/html/booking/tpl/Admin/Resources/manage_resources_access.tpl',
-      1 => 1600849228,
+      1 => 1604966477,
       2 => 'file',
     ),
   ),
@@ -20,18 +20,18 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5fa9bf57661a60_54977481 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5fa9d913824029_51721097 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
 
-<div class="startNotice"
-	 data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMinNotice();?>
+<li class="startNotice"
+	data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMinNotice();?>
 "
-	 data-days="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMinNotice()->Days();?>
+	data-days="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMinNotice()->Days();?>
 "
-	 data-hours="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMinNotice()->Hours();?>
+	data-hours="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMinNotice()->Hours();?>
 "
-	 data-minutes="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMinNotice()->Minutes();?>
+	data-minutes="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMinNotice()->Minutes();?>
 ">
 	<?php if ($_smarty_tpl->tpl_vars['resource']->value->HasMinNotice()) {?>
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'ResourceMinNotice','args'=>$_smarty_tpl->tpl_vars['resource']->value->GetMinNotice()),$_smarty_tpl);?>
@@ -40,15 +40,15 @@ function content_5fa9bf57661a60_54977481 (Smarty_Internal_Template $_smarty_tpl)
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'ResourceMinNoticeNone'),$_smarty_tpl);?>
 
 	<?php }?>
-</div>
-<div class="endNotice"
-	 data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice();?>
+</li>
+<li class="endNotice"
+	data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice();?>
 "
-	 data-days="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice()->Days();?>
+	data-days="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice()->Days();?>
 "
-	 data-hours="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice()->Hours();?>
+	data-hours="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice()->Hours();?>
 "
-	 data-minutes="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice()->Minutes();?>
+	data-minutes="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice()->Minutes();?>
 ">
 	<?php if ($_smarty_tpl->tpl_vars['resource']->value->HasMaxNotice()) {?>
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'ResourceMaxNotice','args'=>$_smarty_tpl->tpl_vars['resource']->value->GetMaxNotice()),$_smarty_tpl);?>
@@ -57,9 +57,9 @@ function content_5fa9bf57661a60_54977481 (Smarty_Internal_Template $_smarty_tpl)
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'ResourceMaxNoticeNone'),$_smarty_tpl);?>
 
 	<?php }?>
-</div>
-<div class="requiresApproval"
-	 data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetRequiresApproval();?>
+</li>
+<li class="requiresApproval"
+	data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetRequiresApproval();?>
 ">
 	<?php if ($_smarty_tpl->tpl_vars['resource']->value->GetRequiresApproval()) {?>
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'ResourceRequiresApproval'),$_smarty_tpl);?>
@@ -68,9 +68,9 @@ function content_5fa9bf57661a60_54977481 (Smarty_Internal_Template $_smarty_tpl)
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'ResourceRequiresApprovalNone'),$_smarty_tpl);?>
 
 	<?php }?>
-</div>
-<div class="autoAssign"
-	 data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetAutoAssign();?>
+</li>
+<li class="autoAssign"
+	data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetAutoAssign();?>
 ">
 	<?php if ($_smarty_tpl->tpl_vars['resource']->value->GetAutoAssign()) {?>
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'ResourcePermissionAutoGranted'),$_smarty_tpl);?>
@@ -79,9 +79,9 @@ function content_5fa9bf57661a60_54977481 (Smarty_Internal_Template $_smarty_tpl)
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'ResourcePermissionNotAutoGranted'),$_smarty_tpl);?>
 
 	<?php }?>
-</div>
-<!-- <div class="enableCheckin"
-	 data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->IsCheckInEnabled();?>
+</li>
+<!-- <li class="enableCheckin"
+	data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->IsCheckInEnabled();?>
 ">
 	<?php if ($_smarty_tpl->tpl_vars['resource']->value->IsCheckInEnabled()) {?>
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'RequiresCheckInNotification'),$_smarty_tpl);?>
@@ -90,14 +90,14 @@ function content_5fa9bf57661a60_54977481 (Smarty_Internal_Template $_smarty_tpl)
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'NoCheckInRequiredNotification'),$_smarty_tpl);?>
 
 	<?php }?>
-</div> --!>
-<div class="autoRelease"
+</li> --!>
+<?php if ($_smarty_tpl->tpl_vars['resource']->value->IsAutoReleased()) {?>
+<li class="autoRelease"
 	 data-value="<?php echo $_smarty_tpl->tpl_vars['resource']->value->GetAutoReleaseMinutes();?>
 ">
-	<?php if ($_smarty_tpl->tpl_vars['resource']->value->IsAutoReleased()) {?>
 		<?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0][0]->SmartyTranslate(array('key'=>'AutoReleaseNotification','args'=>$_smarty_tpl->tpl_vars['resource']->value->GetAutoReleaseMinutes()),$_smarty_tpl);?>
 
-	<?php }?>
-</div>
+</li>
 <?php }
+}
 }
